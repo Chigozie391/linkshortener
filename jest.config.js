c5872f48999e4c7352b95674032e4e0e482a16b3
chigozie391 <chigoziemadubuko@gmail.com>
2021-02-25 01:38:51 +0100

    clean uo

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node"
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/']
 };
