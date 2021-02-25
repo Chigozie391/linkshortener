fe6be07a7dd1837501b38274fb264a8fb5d1969c
chigozie391 <chigoziemadubuko@gmail.com>
2021-02-25 11:21:01 +0100

    seed envs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['<rootDir>/setuptest.ts'],
+  // setupFiles: ['<rootDir>/setuptest.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/']
 };
